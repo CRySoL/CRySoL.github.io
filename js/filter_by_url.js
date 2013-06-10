cb6e0efcfa5aff616c26244bb9618024468547a0
David Villa Alises <david.villa@gmail.com>
2013-06-10 16:57:02 +0200

    pre, kbd style blogs page

--- js/filter_by_url.js
+++ js/filter_by_url.js
@@ -1,6 +1,7 @@
       $(document).ready(function() {{
         selected = location.hash.slice(1);
         if (selected) {
+	  console.log(selected);
           $('div.item').hide();
           $('#' + selected).show();
         }
