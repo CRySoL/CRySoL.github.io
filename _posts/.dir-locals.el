5352dcdbdd392bfe1b94eb360737d1581f6aad5d
David Villa Alises <david.villa@gmail.com>
2013-06-12 20:57:30 +0200

    unknown author

---
+++ _posts/.dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((fundamental-mode
+  (mode . text))
+ (text-mode
+  (mode . visual-line)
+  (eval auto-fill-mode -1)
+  (coding . utf-8)
+  (mode . flyspell)
+  (ispell-local-dictionary . "castellano8")
+  (mode . visual-line)))
