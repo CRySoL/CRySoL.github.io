7c64b404acd94337a83cc7825fca1f231725dfb8
David Villa Alises <david.villa@gmail.com>
2013-06-22 17:57:54 +0200

    about

--- js/filter_by_url.js
+++ js/filter_by_url.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {{
     selected = location.hash.slice(1);
     if (selected) {
-	console.log(selected);
         $('div.item').hide();
         $('#' + selected).show();
     }
