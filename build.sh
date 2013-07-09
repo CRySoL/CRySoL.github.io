56c555bf9d987c89d0bad0300ae66609173e6c64
David Villa Alises <david.villa@gmail.com>
2013-07-09 21:35:51 +0200

    - clone _site if does not exist - new recipe

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash --
 # -*- mode:shell-script; coding:utf-8; tab-width:4 -*-
 
+if [ ! -e _site ]; then
+	git clone git@github.com:CRySoL/CRySoL.github.io _site
+fi
+
 rake migrate_on
 git --git-dir=_site/.git --work-tree=$(pwd)/_site pull
 jekyll build
