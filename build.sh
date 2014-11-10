db0f2e74ee0de3ff69adf1fbb2146a113ef1dc0a
David Villa Alises <david.villa@gmail.com>
2014-11-10 17:12:15 +0100

    clone target repository

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@
 
 set -e
 
-if [  -d _migrate ]; then
+if [ -d _migrate ]; then
 	rake migrate_on
+fi
+
+if ! [ -d _site ];  then
+	git clone git@github.com:CRySoL/CRySoL.github.io.git  _site
 fi
 
 git --git-dir=_site/.git --work-tree=$(pwd)/_site pull
