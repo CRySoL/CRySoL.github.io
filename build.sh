f65848c975ff6e8c36e62a7744842f7d00b924a4
David Villa Alises <david.villa@gmail.com>
2015-10-07 19:17:23 +0200

    automatic compilation

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ git --git-dir=_site/.git --work-tree=$(pwd)/_site pull
 jekyll build
 date >> history.html
 echo "<br/>" >> history.html
+git commit -a -m "automatic compilation"
+git push
 touch _site/.nojekyll
 git --git-dir=_site/.git --work-tree=$(pwd)/_site add --all
 git --git-dir=_site/.git --work-tree=$(pwd)/_site commit -a -m "automatic compilation"
