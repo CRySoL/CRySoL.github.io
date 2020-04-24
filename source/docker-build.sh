b4866814ec8d4344ed35d058be9dd1e05a941c8f
David Villa Alises <david.villa@gmail.com>
2020-04-24 23:42:01 +0200

    docker-build in source

--- source/docker-build.sh
+++ source/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --
 
 docker run \
-       -v $PWD:/srv/jekyll \
+       -v $PWD/source:/srv/jekyll \
        -v "$PWD/vendor/bundle:/usr/local/bundle" \
-       jekyll/jekyll:4.0.0 \
-       /bin/bash -c "chown -R jekyll:jekyll /srv/jekyll && jekyll build --trace -d docs -b crysol.org"
+       jekyll/jekyll:3.8 \
+       /bin/bash -c "chown -R jekyll:jekyll /srv/jekyll && jekyll build --trace -b crysol.org"
