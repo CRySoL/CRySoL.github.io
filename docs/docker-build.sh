3541898d2744e1f22779dbc125bf5db6b1e511ae
David Villa Alises <david.villa@gmail.com>
2020-04-24 23:00:36 +0200

    remove docs

--- docs/docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --
-
-docker run \
-       -v $PWD:/srv/jekyll \
-       -v "$PWD/vendor/bundle:/usr/local/bundle" \
-       jekyll/jekyll:4.0.0 \
-       /bin/bash -c "chown -R jekyll:jekyll /srv/jekyll && jekyll build --trace -d docs -b crysol.org"
--- docs/javascripts/main.js
+++
@@ -1,6 +0,0 @@
-console.log('This would be the main JS file.');
-
-function show_comments() {
-  document.getElementById("disqus_thread").style.display = 'block';
-  document.getElementById("show_comments").style.display = 'none';
-}
