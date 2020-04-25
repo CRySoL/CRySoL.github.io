d722d35d94670b48bbcf1a41eac33b8c26fcc7d7
GitHub Action <action@github.com>
2020-04-25 12:58:21 +0200

    github action jekyll build

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --
 
 docker run \
-       -v $PWD/source:/srv/jekyll \
+       -v $PWD:/srv/jekyll \
        -v "$PWD/vendor/bundle:/usr/local/bundle" \
        jekyll/jekyll:3.8 \
        /bin/bash -c "chown -R jekyll:jekyll /srv/jekyll && jekyll build --trace -b crysol.org"
